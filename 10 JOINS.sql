8c2f8d5899e3b8cd89d539b8278971fb40b71fbd
ABIR MAZUMDAR <abirmazumdar14798@gmail.com>
2025-06-20 22:35:01 +0530

    New Scripts and Modification of scripts were made

--- 10 JOINS.sql
+++ 10 JOINS.sql
@@ -13,7 +13,7 @@ ON student_details.COURSE_ID = course_details.COURSE_ID;
 #************************************** LEFT JOIN ***************************************************
 #INNER JOIN + TUPLES FROM THE LHT WHICH FIALED FOR THE INNER JOIN
 SELECT * 
-FROM student_details LEFT JOIN course_details
+FROM COURSE_DETAILS LEFT JOIN STUDENT_DETAILS
 ON student_details.COURSE_ID = course_details.COURSE_ID;
 
 #************************************** RIGHT JOIN*****************************
@@ -21,5 +21,39 @@ ON student_details.COURSE_ID = course_details.COURSE_ID;
 SELECT * 
 FROM student_details RIGHT JOIN course_details
 ON student_details.COURSE_ID = course_details.COURSE_ID;
+
+#************************************* SELF JOIN ************************************************
+#SELF JOIN IS BEST WHEN WE WANT TO SHOW THE HERICHERY RELATION BETWEEN THE TUPLES
+#EXAMPLE JOB_PROFILE : [COOK , JANITOR , DELIVERY , ASST_MANAGER , MANAGER]
+#COOK , JANITOR AND DELIVERY WILL REPORT TO ASST_MANAGER , AND AST_MANAGER WILL REPORT TO MANAGER
+ 
+select * 
+from table_1;
+
+#ADD A NEW COLUMN IN THE TABLE_1
+ALTER TABLE TABLE_1
+ADD COLUMN REFERAL_ID INT;
+
+#INSERT CERTAIN VALUES INTO IT
+INSERT INTO TABLE_1(AMOUNT,REFERAL_ID)
+VALUES
+(60000.00,103),
+(45000.00,NULL),
+(56000,103);
+
+#UPDATE THE REFER_ID WITH SOME REFERS
+UPDATE TABLE_1
+SET REFERAL_ID=102
+WHERE ID=103 OR ID=105;
+
+#map the ID with the respective ID who refered them
+#SELF JOIN
+SELECT DISTINCT A.REFERAL_ID AS WHO_REFERED,A.ID AS REFERED
+FROM TABLE_1 AS A
+INNER JOIN
+TABLE_1 AS B
+ON
+A.REFERAL_ID=B.REFERAL_ID;
+
 
 
