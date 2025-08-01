c83bbb788f4863117ac939ded73d7aa8bee5177a
ABIR MAZUMDAR <abirmazumdar14798@gmail.com>
2025-08-01 22:40:24 +0530

    FEW CHANGES MADE

--- 13 ORDER_BY.sql
+++ 13 ORDER_BY.sql
@@ -1,6 +1,6 @@
 #*************************** ORDER BY CALUSE***********************
 -- default is Ascending
--- If there is a conflict in arranging the values we can use another column to solve the conflict with its own ASC OR SESC
+-- If there is a conflict in arranging the values we can use another column to solve the conflict with its own ASC OR DESC
 
 SELECT STUDENT_NAME,COURSE_ID
 FROM STUDENT_DETAILS
@@ -13,4 +13,3 @@ FROM BTECH_ADMISSION
 WHERE EMAIL_ID IS NOT NULL
 ORDER BY PAF_AMOUNT DESC ,FIRST_NAME ASC;
 #THE RESULT IS SORTED BASED ON THE PAF_AMOUNT VALUE IF CONFLICT OCUURS THEN WE USE THE FIRST_NAME COLUMN TO RESOLVE IT
-
